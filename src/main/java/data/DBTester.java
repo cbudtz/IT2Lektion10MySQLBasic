30436910e4a183d5df29b7e72b861885f66e4bb1
chbu <chbu@dtu.dk>
2020-04-15 15:17:42 +0200

    minor updates

--- src/main/java/data/DBTester.java
+++ src/main/java/data/DBTester.java
@@ -1,9 +1,6 @@
 package data;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Scanner;
 
 public class DBTester {
@@ -13,10 +10,11 @@ public class DBTester {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Type in lower salary:");
             String salary = scanner.next();
-            Statement statement = conn.createStatement();
+            PreparedStatement statement = conn.prepareStatement("SELECT * FROM instructor WHERE salary > ?");
+            statement.setString(1, salary);
             System.out.println("Connection established");
-
-            ResultSet show_tables = statement.executeQuery("SELECT * FROM instructor WHERE salary > " + salary);
+            conn.setAutoCommit(false);
+            ResultSet show_tables = statement.executeQuery();
             System.out.println("Got a resultset with number of colums:");
             System.out.println(show_tables.getMetaData().getColumnCount());
             while (show_tables.next()){
